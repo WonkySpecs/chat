b8680c03244311d565ecf6d5cd03ac63cd6620a9
Will Taylor <w1ll100@hotmail.co.uk>
2019-11-05 22:27:07 +0000

    Some more poorly structured js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 window.onload = function() {
     console.log("Starting");
+    ui = loadUI();
+    console.log(ui);
+    ui.sendBtn.onclick = ev => console.log(ui.messageInput.value);
     var ws = new WebSocket("ws://127.0.0.1:5050/");
     ws.onopen = () => ws.send(create());
     ws.onmessage = function (msg) {
+        console.log(msg);
         if(msg.data === undefined) {
             // Error handling
             return
         }
 
-        let message = msg.data
-            switch(message.type) {
-                case "message":
-                    messageReceived(message);
-                    break;
-                case "joined":
-                    joinedRoom(message);
-                    break;
-                case "created":
-                    createdRoom(message);
-                    break;
-                default:
-                    console.log("Unkown message type: " + message.type);
-            }
+        let message = JSON.parse(msg.data);
+        switch(message.type) {
+            case "message":
+                messageReceived(message);
+                break;
+            case "joined":
+                joinedRoom(message);
+                break;
+            case "created":
+                createdRoom(message);
+                break;
+            default:
+                console.log("Unkown message type: " + message.type);
+        }
     };
 }
+
 function messageReceived(msg) {
     // Get message window from container
     // Format text
@@ -41,6 +46,18 @@ function joinedRoom(msg) {
 function createdRoom(msg) {
     // Same as joined
     console.log("Created room", msg);
+}
+
+function loadUI() {
+    sendBtn = document.querySelector("#messageInputContainer button");
+    messageInput = document.querySelector("#messageInputContainer input");
+    return {
+        "sendBtn": sendBtn,
+        "messageInput": messageInput,
+        "tabContainer": document.getElementById("chatTabContainer"),
+        "members": document.getElementById("chatMembers"),
+        "messageWindows": document.getElementById("messageWindowContainer")
+    }
 }
 create = () => JSON.stringify({"message_type": "create"});
 join = room_id => JSON.stringify({"message_type": "join", "room_id": room_id});
