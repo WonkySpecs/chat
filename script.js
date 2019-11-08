7cfded3cc97106c45a5b82e08f07fd017fe1fd1a
Will Taylor <w1ll100@hotmial.co.uk>
2019-11-08 16:28:47 +0000

    More bits

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ window.onload = function() {
     console.log(ui);
     ui.sendBtn.onclick = ev => console.log(ui.messageInput.value);
     var ws = new WebSocket("ws://127.0.0.1:5050/");
-    ws.onopen = () => ws.send(create());
+    ws.onopen = () => ws.send(buildMessage.join("4WSUSO"));
     ws.onmessage = function (msg) {
         console.log(msg);
         if(msg.data === undefined) {
-            // Error handling
+	    console.log("Message without data received", msg);
             return
         }
 
         let message = JSON.parse(msg.data);
         switch(message.type) {
             case "message":
-                messageReceived(message);
+                messageReceived(message, ui);
                 break;
             case "joined":
-                joinedRoom(message);
+                room = joinedRoom(message, ui);
                 break;
             case "created":
-                createdRoom(message);
+                createdRoom(message, ui);
                 break;
             default:
                 console.log("Unkown message type: " + message.type);
@@ -29,7 +29,7 @@ window.onload = function() {
     };
 }
 
-function messageReceived(msg) {
+function messageReceived(msg, ui) {
     // Get message window from container
     // Format text
     // Add message to window
@@ -37,13 +37,13 @@ function messageReceived(msg) {
     console.log("Message received", msg);
 }
 
-function joinedRoom(msg) {
-    // Create new tab for room
-    // Switch to it + repopulate members?
+function joinedRoom(msg, ui) {
     console.log("Joining room", msg);
+    newTab = 123;
+    return new Room(msg.room_id, newTab, bindMessageTabWrapper(ui.messageWindow));
 }
 
-function createdRoom(msg) {
+function createdRoom(msg, ui) {
     // Same as joined
     console.log("Created room", msg);
 }
@@ -52,13 +52,44 @@ function loadUI() {
     sendBtn = document.querySelector("#messageInputContainer button");
     messageInput = document.querySelector("#messageInputContainer input");
     return {
-        "sendBtn": sendBtn,
-        "messageInput": messageInput,
-        "tabContainer": document.getElementById("chatTabContainer"),
-        "members": document.getElementById("chatMembers"),
-        "messageWindows": document.getElementById("messageWindowContainer")
+        sendBtn: sendBtn,
+        messageInput: messageInput,
+        tabContainer: document.getElementById("chatTabContainer"),
+        members: document.getElementById("chatMembers"),
+        messageWindow: document.getElementById("messageWindowContainer")
     }
 }
-create = () => JSON.stringify({"message_type": "create"});
-join = room_id => JSON.stringify({"message_type": "join", "room_id": room_id});
-message = (room_id, msg) => JSON.stringify({"message_type": "message", "room_id": room_id, "data": msg});
+
+buildMessage = {
+    create: () => JSON.stringify({"message_type": "create"}),
+    join: room_id => JSON.stringify({"message_type": "join", "room_id": room_id}),
+    message: (room_id, msg) => JSON.stringify({"message_type": "message", "room_id": room_id, "data": msg}),
+}
+
+class Room {
+    constructor(id, tab, bindMessageTab) {
+        this.id = id;
+        this.tab = tab;
+        this.messages = [];
+        this.tab.onclick = bindMessageTab(this.messages)
+    }
+
+    pushMessage(msg) {
+        this.messages.push(msg);
+    }
+}
+
+function bindMessageTabWrapper(messageWindow) {
+    return function(messages) {
+        setMessageWindowContents(messageWindow, messages);
+    }
+}
+
+function setMessageWindowContents(messageWindow, messages) {
+    messageWindow.innerHTML = "";
+    messages.forEach(function(msg) {
+        msgElement = document.createNode("span");
+        msgElement.textContent = msg;
+        messageWindow.appendChild(msgElement);
+    });
+}
