bed5d8e889ff8c198064195682b55163f9d0a79a
Will Taylor <w1ll100@hotmail.co.uk>
2019-11-09 12:33:11 +0000

    Correct kwarg name for ErrorMessage

--- server.py
+++ server.py
@@ -28,20 +28,20 @@ async def handle_message(message: str, user_id: UUID) -> Optional[Message]:
     elif message.type == MessageType.JOIN_ROOM.value:
         print(f"Joining room {message.room_id}")
         if message.room_id not in rooms:
-            return ErrorMessage(response_code=ResponseCode.NOT_FOUND, message=f"Room {message.room_id} does not exist")
+            return ErrorMessage(response_code=ResponseCode.NOT_FOUND, data=f"Room {message.room_id} does not exist")
         rooms[message.room_id].join(user_id)
         return Message(message_type=MessageType.JOINED, room_id=message.room_id)
 
     elif message.type == MessageType.MESSAGE.value:
         if message.room_id not in rooms:
-            return ErrorMessage(response_code=ResponseCode.NOT_FOUND, message=f"Room {message.room_id} does not exist")
+            return ErrorMessage(response_code=ResponseCode.NOT_FOUND, data=f"Room {message.room_id} does not exist")
         print(f"Message sent to room {message.room_id}")
         response_message = json.dumps(Message(message_type=MessageType.MESSAGE, room_id=message.room_id, data=message.data).__dict__)
         for user_id in rooms[message.room_id].members:
             print(f"To: {user_id}")
             await connections[user_id].send(response_message)
         return None
-    return ErrorMessage(response_code=ResponseCode.BAD_REQUEST, message=f"Unkown message_type {message.type}")
+    return ErrorMessage(response_code=ResponseCode.BAD_REQUEST, data=f"Unkown message_type {message.type}")
 
 async def connect(websocket, path):
     user_id = uuid4()
